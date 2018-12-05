e9a04d9168063726b73567e976bac1e4635b5ef3
AKD47 <van83horn@gmail.com>
2018-12-05 13:29:07 +0300

    main page mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    /*mobile menu*/
+    $(document).on('click', '#mobile-menu', function (event) {
+        event.preventDefault();
+        var menu = $(this).closest('.header__navigation').find('.header__nav');
+        if ($(this).hasClass('js-trigger-active')) {
+            $(this).removeClass('js-trigger-active');
+            menu.slideUp('slow');
+        } else {
+            $(this).addClass('js-trigger-active');
+            menu.slideDown('slow');
+        }
+        return false;
+    });
+    /*close mobile menu*/
 });
 
 window.onload = function () {
